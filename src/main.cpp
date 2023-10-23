5f3c3392d2d66cff97cd7924d702bfeda831c51f
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-10-23 22:20:24 +0700

    feat: add servo library and move milis to library milisDelay
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <Adafruit_ST7789.h>
 #include <Arduino.h>
 #include <DFRobotDFPlayerMini.h>
+#include <ESP32Servo.h>
 #include <PN532.h>
 #include <PN532_HSU.h>
 #include <SPI.h>
 #include <SoftwareSerial.h>
+#include <millisDelay.h>
 #include <qrcode.h>
 
 // Declare PIN TFT
@@ -22,6 +24,9 @@
 #define METAL_SENSOR_PIN 39
 #define CANCEL_BUTTON_PIN 22
 
+// Otput PIN
+#define SERVO_PIN 13
+
 // Declare Color
 #define ST77XX_DARK_GRAY 0x4228
 
@@ -33,6 +38,7 @@ PN532_HSU pn532shu(Serial1);
 PN532 nfc(pn532shu);
 SoftwareSerial softwareSerial(SS_RX_PIN, SS_TX_PIN);
 DFRobotDFPlayerMini dfPlayer;
+Servo servo;
 
 int dotIndex = 0;
 bool isLoading = false;
@@ -41,12 +47,21 @@ String loadingText = "Loading...";
 unsigned long previousMillis = 0;
 const long interval = 100;
 
+const unsigned long LOADING_DELAY = 100;
+millisDelay loadingDelay;
+
+const unsigned long SERVO_DELAY = 3000;
+const unsigned long SERVO_WAITING_DELAY = 400;
+millisDelay servoDelay;
+
 void drawProgressBar();
 void displayCenteredText(String text, uint8_t textSize);
 void displayQRCode(String text);
 void readRFIDAndNFC();
 
-void setup(void) {
+int pos = 0;
+
+void setup() {
   Serial.begin(115200);
   Serial.print(F("Hello World!"));
   tft.init(240, 240, SPI_MODE3);
@@ -62,6 +77,7 @@ void setup(void) {
   pinMode(CANCEL_BUTTON_PIN, INPUT);
   pinMode(IR_SENSOR_PIN, INPUT);
   pinMode(METAL_SENSOR_PIN, INPUT);
+  servo.attach(SERVO_PIN);
 
   softwareSerial.begin(9600);
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
@@ -101,6 +117,9 @@ void setup(void) {
   delay(1000);
   displayQRCode("https://www.google.com");
   delay(5000);
+
+  loadingDelay.start(LOADING_DELAY);
+  // servoDelay.start(SERVO_DELAY);
 }
 
 void loop() {
@@ -118,15 +137,27 @@ void loop() {
 
   // Serial.println(analogRead(IR_SENSOR_PIN));
   // Serial.println(analogRead(METAL_SENSOR_PIN));
-  readRFIDAndNFC();
+  // readRFIDAndNFC();
+
+  // if (servoDelay.justFinished()) {
+  //   static bool isBack = true;
+  //   if (isBack) {
+  //     servo.write(180);
+  //     servoDelay.start(SERVO_WAITING_DELAY);
+  //     isBack = false;
+  //   } else {
+  //     servo.write(0);
+  //     servoDelay.stop();
+  //     isBack = true;
+  //   }
+  // }
 }
 
 void drawProgressBar() {
   displayCenteredText(loadingText, DEFAULT_TEXT_SIZE);
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+  if (loadingDelay.justFinished()) {
+    loadingDelay.repeat();
 
     const int numDots = 10;
     const int dotSpacing = 12;
